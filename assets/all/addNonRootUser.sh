7b841b4f82c4796bb638f91fc62c83598f7473bb
Corbin Champion <corbinlc@gmail.com>
2023-03-10 19:07:13 -0800

    clean up term settings

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -11,7 +11,9 @@ if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
 fi
 
 if [ ! -d "/home/$INITIAL_USERNAME" ]; then
+  sed -i 's/force_color_prompt=/#force_color_prompt=/g' /etc/skel/.bashrc
   useradd $INITIAL_USERNAME -s /bin/bash -m -u 2000 
   echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
   chsh -s /bin/bash $INITIAL_USERNAME
+  touch /home/$INITIAL_USERNAME/.hushlogin
 fi
